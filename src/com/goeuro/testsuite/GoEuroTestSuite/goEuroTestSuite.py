538145d0a10d127b9b8ec926915202131337956f
Saritha Rajagopal <saripr@gmail.com>
2015-07-03 02:53:37 +0530

    Test Search functionality-Source and destination are different
    Search functionality is tested when source and destination are
    different. Add a function which accepts two different user inputs for
    searching.

--- src/com/goeuro/testsuite/GoEuroTestSuite/goEuroTestSuite.py
+++ src/com/goeuro/testsuite/GoEuroTestSuite/goEuroTestSuite.py
@@ -5,12 +5,14 @@ Description:Checks the sorting of travel cost between two locations
 '''  
 
 import unittest
-
+import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+
 '''
     Global variables are created for source and destination locations.
 '''
@@ -59,6 +61,22 @@ class goEuroTestSuite(unittest.TestCase):
         expected_alert_msg='Please enter two different locations.'
         WebDriverWait(driver,10).until(EC.text_to_be_present_in_element((By.XPATH,message), expected_alert_msg))
     
+    '''
+    Checks the search functionality by entering different source and destination locations. 
+    '''      
+    def test_main_page_search_positive(self):
+        driver=self.driver
+        time.sleep(5)             
+        self.source_dest_entry(BERLIN,PRAGUE)
+        search_button = driver.find_element_by_id('search-form__submit-btn')
+        driver.implicitly_wait(8)    
+        search_button.click()
+        try:
+            WebDriverWait(driver,8).until(EC.text_to_be_present_in_element_value((By.ID,'from_filter'),BERLIN))
+            WebDriverWait(driver,8).until(EC.text_to_be_present_in_element_value((By.ID,'to_filter'),PRAGUE))
+        except TimeoutException:
+            assert False,"Timeout exception raised" 
+        driver.implicitly_wait(10)  
 
     '''
        Exits the entire browser.
